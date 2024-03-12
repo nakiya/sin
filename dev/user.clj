2b83b072d297ff16aacc0bd8be8a97179f148d5f
Duminda Rathnayaka <dumrat@gmail.com>
2024-03-12 12:09:05 +0800

    Initial commit

---
+++ dev/user.clj
@@ -0,0 +1,27 @@
+(ns user
+  (:require [portal.api :as portal]
+            [clojure.datafy :as d]))
+
+(portal/start {:portal.colors/theme :portal.colors/gruvbox
+               :port 45001
+               :host "localhost"})
+
+(defn error->data [ex]
+  (assoc (d/datafy ex) :runtime :clj))
+
+(defn submit [value]
+  (portal/submit
+   (if-not (instance? Exception value)
+     value
+     (error->data value))))
+
+(println "Adding tap")
+(add-tap #'submit)
+
+(tap> [:welcome!])
+
+(comment
+
+  (+ 1 2)
+
+  (tap> (Exception. "e")))
